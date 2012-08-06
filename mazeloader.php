b2babd1eb25ed58bfe23fa321a876c4f1b2949a9
Andrew <andrew@andrew-mint.(none)>
2012-08-06 19:37:00 -0400

    changes to ajax request

--- mazeloader.php
+++ mazeloader.php
@@ -2,6 +2,21 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+function strip_non_numeric($x) {
+    return preg_replace('/\D/', '', $x);
+}
+
+function get_coordinates($x) {
+    $x[0] = strip_non_numeric($x[0]);
+    $x[1] = strip_non_numeric($x[1]);
+    return $x;
+}
+
+function add_coordinates(&$maze, $var, $array) {
+    $maze->$var->x = $array[0];
+    $maze->$var->y = $array[1];
+}
+
 class Maze {}
 
 class MazeReader {
@@ -15,10 +30,27 @@ class MazeReader {
 			$mazetext[$i] = str_split($mazetext[$i]);
 		}
 		$maze = new Maze();
-		$maze->start = $mazetext[45];
-		$maze->goal = $mazetext[46];
-		$maze->recharger = $mazetext[47];
+        
+        //start
+        $start = explode(',', $mazetext[45]);
+        $start = get_coordinates($start);
+        add_coordinates($maze, "start", $start);
+        $maze->start->dir = $start[2];
+        
+        //goal
+        $goal = explode(',', $mazetext[46]);
+        $goal = get_coordinates($goal);
+        add_coordinates($maze, "goal", $goal);
+        
+        //recharger
+        $recharger = explode(',', $mazetext[47]);
+        $recharger = get_coordinates($recharger);
+        add_coordinates($maze, "recharger", $recharger);
+        
+        //maze
 		$maze->maze = array_slice($mazetext, 1, 44);
+        
+        //return json array
 		echo json_encode($maze);
 	}
 }
