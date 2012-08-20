bb9152f030aaa74763861e92fc9b5e11aaac7617
Andrew <andrew@andrew-desktop-mint.(none)>
2012-08-20 19:28:39 -0400

    need to fix rotation it just goes nuts!

--- js/Bot.js
+++ js/Bot.js
@@ -16,7 +16,20 @@ Bot = function (maze) {
 	this.sprite = new lime.Sprite().setFill(Constants.Graphics.IMG_ASSETS + 'bot.png');
 	
     // private functions
-    
+    updateDirection = function(x) {
+		self.sprite.setAnchorPoint(0.5, 0.5);
+		self.sprite.runAction(
+			new lime.animation.Sequence(
+				new lime.animation.ScaleTo(1.2).setDuration(.2),
+				new lime.animation.RotateBy(x),
+				new lime.animation.ScaleTo(1).setDuration(.2)
+			)
+		);
+		//lime.scheduleManager.callAfter(function() {self.sprite.setAnchorPoint(0, 0);}, null, 1)
+    }
+    updatePosition = function() {
+		self.sprite.runAction(new lime.animation.MoveTo(getScreenPosition()).setSpeed(0.5));
+    }
     getScreenPosition = function() {
 		// for whatever reason the position.coord vars are detected as strings here... stupid JS
         var coord = new goog.math.Coordinate(Constants.Graphics.CELL_W * position.x*1, Constants.Graphics.CELL_H * position.y*1);
@@ -31,24 +44,31 @@ Bot = function (maze) {
 			case MOVE.FORWARD:
 				if (maze.get(sum(position, direction)) == Constants.Maze.OPEN) {
 					position = sum(position, direction);
-					console.log(position);
+					updatePosition();
 				}
 			break;
 			case MOVE.BACKWARD:
 				if (maze.get(difference(position, direction)) == Constants.Maze.OPEN) {
 					position = difference(position, direction);
-					console.log(position);
+					updatePosition();
 				}
 			break;
 		}
 	}
 	this.turn = function(dir) {
+		var rotate = 0;
+		console.log(direction);
 		switch(dir) {
 			case TURN.RIGHT:
+				rotate = 90*180/Math.PI;
 			break;
 			case TURN.LEFT:
+				rotate = -90*180/Math.PI;
 			break;
 		}
+		direction = direction.rotate(rotate);
+		console.log(direction);
+		updateDirection(rotate);
 	}
 	this.look = function(die) {
 		switch(dir) {
@@ -74,7 +94,7 @@ Bot = function (maze) {
     
     //AnchorPoint is defined with ‘setAnchorPoint()’ method. The parameters are vector points in 0 to 1 range where (0,0) means top-left and (1,1) bottom right corner. By default all elements are positioned from the center and so have anchor point set to (0.5,0.5).
 	this.sprite.setAnchorPoint(0, 0);
-    this.sprite.setPosition(getScreenPosition());
+    updatePosition();
 
 	// from old code - move this to new style
 	//bot.sprite.runAction(new lime.animation.Sequence(
