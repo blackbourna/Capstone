849bc342a9d9315a7d62eecf38beed99a5d523d6
Andrew <andrew@andrew-desktop-mint.(none)>
2012-09-02 13:28:12 -0400

    attempt to get it on dotcloud

--- app/CapstoneProject_BlackbournA.php
+++ app/CapstoneProject_BlackbournA.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<title>CapstoneProject_BlackbournA</title>
-	<script type="text/javascript" src="../../closure/closure/goog/base.js"></script>
+	<script type="text/javascript" src="../../../closure/closure/goog/base.js"></script>
 	<script type="text/javascript" src="js/CapstoneProject_BlackbournA.js"></script>
 </head>
 
