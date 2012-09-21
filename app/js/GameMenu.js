2f88d8ea2fc699a877594e417f11765c73d858d7
Andrew <andrew@andrew-mint.(none)>
2012-09-21 10:13:37 -0400

    fixed mazemenu

--- app/js/GameMenu.js
+++ app/js/GameMenu.js
@@ -9,19 +9,19 @@ GameMenu=function(director) {
 	this.showMenu = function() {
 		var scene = new lime.Scene();
 		scene.setRenderer(lime.Renderer.CANVAS);
-		var startGameButton = new lime.GlossyButton('Start Game').setPosition(500, 500).setSize(500, 50);
+		var startGameButton = new lime.GlossyButton('Start Game').setPosition(500, 400).setSize(500, 50);
 		goog.events.listen(startGameButton, ['mousedown','touchstart'], function(e) {
 			new MazeMenu(director).showMenu();
 		});
 		scene.appendChild(startGameButton);
 
-		var helpButton = new lime.GlossyButton('Instructions').setPosition(500, 600).setSize(500, 50);
+		var helpButton = new lime.GlossyButton('Instructions').setPosition(500, 500).setSize(500, 50);
 		goog.events.listen(helpButton, ['mousedown','touchstart'], function(e) {
 			
 		});
 		scene.appendChild(helpButton);
 
-		var highscoreButton = new lime.GlossyButton('High Scores').setPosition(500, 700).setSize(500, 50);
+		var highscoreButton = new lime.GlossyButton('High Scores').setPosition(500, 600).setSize(500, 50);
 		goog.events.listen(highscoreButton, ['mousedown','touchstart'], function(e) {
 
 		});
