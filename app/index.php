f25f462f2d16272996b89762313a7695d98fbc75
Andrew <andrew@andrew-desktop-mint.(none)>
2012-10-19 18:25:24 -0400

    more dotcloud

--- app/index.php
+++ app/index.php
@@ -1,1 +1,6 @@
-<?php include('CapstoneProject_BlackbournA.php'); ?>
+<?php 
+if (strlen($_REQUEST['c'])):
+  include('compiled.php'); 
+else:
+  include('CapstoneProject_BlackbournA.php'); 
+endif; ?>
